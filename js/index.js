1d215bbdada8f62f7dfc725e0121bc896216f71a
modaoudi <daoudi.mreda@gmail.com>
2017-11-25 19:53:42 +0100

    js/jquery

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 var generate_country = function (name) {
+    var vids = $('#videos');
+    vids.html('');
+    $('#country').html(name);
     $.getJSON("json/countries.json", function(json) {
-        console.log(json[name]); // this will show the info it in firebug console
+        var videos = json[name];
+        $('#country').html(name);
+        for (var id in videos) {
+            vids.append("<p>" + id + "</p>")
+        }
     });
 };
