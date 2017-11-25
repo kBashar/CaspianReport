8a3dcbd6faab2585d426f11cf6c67dc055c278f3
modaoudi <daoudi.mreda@gmail.com>
2017-11-25 19:19:05 +0100

    countries.json, css, sidebar

---
+++ js/index.js
@@ -0,0 +1,5 @@
+var generate_country = function (name) {
+    $.getJSON("../json/countries.json", function(json) {
+        console.log(json); // this will show the info it in firebug console
+    });
+};
--- scripts/generate.py
+++ scripts/generate.py
@@ -2,17 +2,22 @@ import json
 import requests
 
 api_key = json.load(open("../json/api.json"))["key"]
-api_url = "https://www.googleapis.com/youtube/v3/search?" \
+list_url = "https://www.googleapis.com/youtube/v3/search?" \
       "part=snippet&channelId=UCwnKziETDbHJtx78nIkfYug&maxResults=50&pageToken={PT}&type=video&key={AK}"
+info_url = "https://www.googleapis.com/youtube/v3/videos?part=snippet&id={ID}&key={AK}"
 
 
 def get_list(token=""):
-    full_url = api_url
+    full_url = list_url
     if token:
         full_url = full_url.replace("{PT}", token)
     else:
         full_url = full_url.replace("&pageToken={PT}", "")
     return requests.get(full_url.replace("{AK}", api_key)).json()
+
+
+def get_info(video_id):
+    return requests.get(info_url.replace("{ID}", video_id).replace("{AK}", api_key)).json()
 
 
 def generate():
@@ -25,14 +30,35 @@ def generate():
     return videos
 
 
+def separate(videos):
+    countries = json.load(open('../json/countries.json'))
+    for video in videos:
+        info = get_info(video)
+        for country in countries:
+            if country in info["items"][0]["snippet"]["tags"]:
+                countries[country].append(video)
+    with open('../json/countries.json', 'w') as outfile:
+        json.dump(countries, outfile, indent=4)
+    return
+
+
+def clean_countries():
+    countries = json.load(open('../json/countries.json'))
+    for country in countries:
+        countries[country] = []
+    with open('../json/countries.json', 'w') as outfile:
+        json.dump(countries, outfile, indent=4)
+
+
 def add_to_dict(begin, end):
     for video in begin:
         end[video["id"]["videoId"]] = video["snippet"]["title"]
 
 
 if __name__ == "__main__":
-    print(api_key)
-    with open('../json/videos.json', 'w') as fp:
-        json.dump(generate(), fp, indent=4)
+    clean_countries()
+    separate(generate())
+
+
 
 
